7f35fa818e7e690d8fad746e349e5410dfffe3d9
developertanvir2019 <developertanvir2019@gmail.com>
2023-07-30 17:16:44 +0600

    dynamic loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
-import Banner from "@/components/UI/Banner";
+// import Banner from "@/components/UI/Banner";
 import AllNews from "@/components/UI/AllNews";
 import { useGetNewsesQuery } from "@/redux/api/api";
-
+import dynamic from 'next/dynamic'
 
 const HomePage = ({ allNews }) => {
   const { data } = useGetNewsesQuery();
+
+  const DynamicBanner = dynamic(() => import('@/components/UI/Banner'), {
+    loading: () => <h1>Loading...</h1>,
+    ssr: false
+  })
   return (
     <>
       <Head>
@@ -18,7 +23,7 @@ const HomePage = ({ allNews }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Banner />
+      <DynamicBanner />
       {/* <AllNews allNews={allNews} /> */}
       <AllNews allNews={data} />
     </>
